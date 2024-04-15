22ef810a16f5efee8f04593a36582d9933803f6a
H3Airness <damienraunier@gmail.com>
2024-04-15 15:17:33 +0200

    github-actions-demo v3

--- job.py
+++ job.py
@@ -1,2 +1,10 @@
-a = 2
-print("coucou", a)
+import pytest
+
+from mon_module import calculer_moyenne
+
+def test_calculer_moyenne():
+    valeurs = [10, 20, 30, 40, 50]
+
+    moyenne = calculer_moyenne(valeurs)
+
+    assert moyenne == 30
